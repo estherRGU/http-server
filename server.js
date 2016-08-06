263322b8a9ca2477f04531f5641fdacad50f650c
ESTHER <e.w.koloh@rgu.ac.uk>
2016-08-06 16:02:03 +0100

    initial commit2

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-// load the express package and create our app
-var express = require('express');
-var app     = express();
+// get the http and filesystem modules
+var http = require('http')
+fs = require('fs');
 
-// set the port based on environment (more on environments later)
-var port    = 1337;
+// create our server using the http module
+http.createServer(function(req, res) {
 
-// send our index.html file to the user for the home page
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+    // write to our server. set configuration for the response
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Access-Control-Allow-Origin' : '*'
+    });
 
-// start the server
-app.listen(1337);
-console.log('1337 is the magic port!');
+    var readStream = fs.createReadStream(__dirname + '/index.html');
+
+    // send a message
+    readStream.pipe(res);
+
+}).listen(1337);
+
+// tell ourselves what's happening
+console.log('Visit me at http://localhost:1337');
